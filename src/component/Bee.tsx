ca2f0109d9603b9dafbf333ead6271b05971d1b2
Le Huy Van 20176906 <van.lh176906@sis.hust.edu.vn>
2022-02-23 07:17:12 +0700

    alo

--- src/component/Bee.tsx
+++ src/component/Bee.tsx
@@ -3,21 +3,60 @@ import Bee from '../database/bee.model';
 import Queen from '../database/queen.model';
 import BeeStatus from './BeeStatus';
 import BeeUI from './BeeUI';
+// import Bee from '../database/bee.model';
+// import Queen from '../database/queen.model';
+// import BeeStatus from './BeeStatus';
+// import BeeUI from './BeeUI';
 
-function OneBee(prop: { bee: Bee }) {
+// function OneBee(prop: { bee: Bee }) {
    
-    const [beeStatus,setBeeStatus] = useState("Queen");
+//     const [beeStatus,setBeeStatus] = useState("Queen");
     
-    return (
+//     return (
+//         <tr>
+//             <td>{prop.bee.id}</td>
+//             <td>{prop.bee.hp}</td>
+//             <td>{getType(prop.bee)}</td>
+//             <td><BeeStatus hp={prop.bee.hp}/></td>
+//             <td><BeeUI bee={prop.bee}/></td>
+//         </tr>
+//     )
+// }
+// const getType=(bee: Bee) => {
+//     if(bee instanceof Queen) {
+//         return "Queen";
+//     }else if(bee instanceof Worker) {
+//         return "Worker";
+//     }else {
+//         return "Drone"
+//     }
+
+// }
+
+// export default OneBee;
+
+export interface BeeProps {
+    bee: Bee;
+  };
+
+export class OneBee extends React.Component<BeeProps> {
+    // state: MyState = {
+    //   // optional second annotation for better type inference
+    //   count: 0,
+    // };
+    render() {
+      return (
         <tr>
-            <td>{prop.bee.id}</td>
-            <td>{prop.bee.hp}</td>
-            <td>{getType(prop.bee)}</td>
-            <td><BeeStatus hp={prop.bee.hp}/></td>
-            <td><BeeUI bee={prop.bee}/></td>
-        </tr>
-    )
-}
+                   <td>{this.props.bee.id}</td>
+                   <td>{this.props.bee.hp}</td>
+                   <td>{getType(this.props.bee)}</td>
+                   <td><BeeStatus hp={this.props.bee.hp}/></td>
+                   <td><BeeUI bee={this.props.bee}/></td>
+               </tr>
+      );
+    }
+  }
+
 const getType=(bee: Bee) => {
     if(bee instanceof Queen) {
         return "Queen";
@@ -28,7 +67,3 @@ const getType=(bee: Bee) => {
     }
 
 }
-
-export default OneBee;
-
-
--- src/component/BeeUI.tsx
+++ src/component/BeeUI.tsx
@@ -1,23 +1,104 @@
+import React from "react";
+import { SuperProperty } from "typescript";
 import Bee from "../database/bee.model";
 import Queen from "../database/queen.model";
 import Worker from "../database/worker.model";
+import { BeeProps } from "./Bee";
+// export default function BeeUI(prop: {
+//   bee: Bee
+// }) {
+//   let src = '';
+//   if (prop.bee instanceof Queen) {
+//     src = process.env.PUBLIC_URL + '/crown.png';
+//   } else if (prop.bee instanceof Worker) {
+//     src = process.env.PUBLIC_URL + '/bucket.png';
+//   } else {
+//     src = process.env.PUBLIC_URL + '/sword.png';
+//   }
+//   return (
+
+
+//     <div className='bee-ui'>
+//       <img className='bee-img' src={process.env.PUBLIC_URL + '/bee.png'} alt="Bee Image"></img>
+//       <img className='bee-item-img' src={src} alt="Bee Image"></img>
+//     </div>
+//   )
+// }
+
+
+interface QueenProps extends BeeProps {
+  queenItem: string;
+};
+interface DroneProps extends BeeProps {
+  droneItem: string;
+};
+interface WorkerProps extends BeeProps {
+  workerItem: string;
+};
+export class DroneUI extends React.Component<DroneProps> {
+  render() {
+   
+      return (
+        <div className='bee-ui'>
+          <img className='bee-img' src={process.env.PUBLIC_URL + '/bee.png'} alt="Bee Image"></img>
+          <img className='bee-item-img' src={this.props.droneItem} alt="Bee Image"></img>
+        </div>
+      
+    );
+  }
+}
+export class WorkerUI extends React.Component<WorkerProps> {
+  render() {
+   
+      return (
+        <div className='bee-ui'>
+          <img className='bee-img' src={process.env.PUBLIC_URL + '/bee.png'} alt="Bee Image"></img>
+          <img className='bee-item-img' src={this.props.workerItem} alt="Bee Image"></img>
+        </div>
+      
+    );
+  }
+}
+
+export class QueenUI extends React.Component<QueenProps> {
+  render() {
+   
+      return (
+        <div className='bee-ui'>
+          <img className='bee-img' src={process.env.PUBLIC_URL + '/bee.png'} alt="Bee Image"></img>
+          <img className='bee-item-img' src={this.props.queenItem} alt="Bee Image"></img>
+        </div>
+      
+    );
+  }
+}
+
+
+
 export default function BeeUI(prop: {
   bee: Bee
 }) {
-  let src = '';
+
+
+  // return (
+
+
+  //   <div className='bee-ui'>
+  //     <img className='bee-img' src={process.env.PUBLIC_URL + '/bee.png'} alt="Bee Image"></img>
+  //     <img className='bee-item-img' src={""} alt="Bee Image"></img>
+  //   </div>
+  // )
+
+  // let src = '';
   if (prop.bee instanceof Queen) {
-    src = process.env.PUBLIC_URL + '/crown.png';
+    return <QueenUI queenItem= {"process.env.PUBLIC_URL + '/crown.png"}/>;
   } else if (prop.bee instanceof Worker) {
-    src = process.env.PUBLIC_URL + '/bucket.png';
+    return WorkerUI;
   } else {
-    src = process.env.PUBLIC_URL + '/sword.png';
+    return DroneUI;
+    // src = process.env.PUBLIC_URL + '/sword.png';
   }
-  return (
 
-
-    <div className='bee-ui'>
-      <img className='bee-img' src={process.env.PUBLIC_URL + '/bee.png'} alt="Bee Image"></img>
-      <img className='bee-item-img' src={src} alt="Bee Image"></img>
-    </div>
-  )
+    
 }
+
